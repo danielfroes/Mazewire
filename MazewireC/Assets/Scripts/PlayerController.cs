1fce40bc263115b18b6e94c214da578929354013
Pedro Oliveira <pedro.oli.95@outlook.com>
2019-11-02 20:49:51 -0300

    Revert "adicionado o ataque (e um placeholder de inimigo pra testar o ataque)"
    This reverts commit a07ce784a1d83868ab7bbbd74830ba79f6b2ffb7.

--- MazewireC/Assets/Scripts/PlayerController.cs
+++ MazewireC/Assets/Scripts/PlayerController.cs
@@ -6,28 +6,22 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
 
-    private float timeBetweenAttack;
-    public float startTimeBetweenAttack;
-    public float attackRangeX;
-    public float attackRangeY;
-    public int damage;
-    public float speed;
-    public float jumpForce;
-    public float fallMultiplier;
-    public float lowJumpMultiplier;
-    public float dashSpeed;
+    public float speed = 10f;
+    public float jumpForce = 8f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float dashSpeed = 20f;
 
-    private bool facingRight = true;
     public bool isGrounded;
     public bool hasDashed;
     public static bool canMove = true;
 
-    public LayerMask whatIsEnemies;
-    public Transform attackPosition;
     public Transform groundCheck;
     public Transform groundCheckL;
     public Transform groundCheckR;
+    
 
+    private SpriteRenderer spriteR;
     private Animator anim;
 
     // Start is called before the first frame update
@@ -35,6 +29,8 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        spriteR = GetComponent<SpriteRenderer>();
+
     }
 
     private void FixedUpdate()
@@ -58,33 +54,18 @@ public class PlayerController : MonoBehaviour
         Vector2 dir = new Vector2(x, y);
         Walk(dir);
 
-        // jump:
+
         if(isGrounded && Input.GetKeyDown(KeyCode.Z) && canMove)
         {
             Jump();
         }
 
-        // dash:
         if(Input.GetKeyDown(KeyCode.X) && !hasDashed && canMove)
         {
             if(xRaw != 0 || yRaw != 0)
                 Dash(xRaw, yRaw);
         }
 
-        // attack:
-        if(timeBetweenAttack <= 0)
-        {
-            if(Input.GetKeyDown(KeyCode.C)){
-                Attack();
-                timeBetweenAttack = startTimeBetweenAttack;
-            }
-        }
-        else
-        {
-            timeBetweenAttack -= Time.deltaTime;
-        }
-
-        // velocity update:
         if(rb.velocity.y < 0 && canMove)
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
@@ -95,12 +76,6 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(attackPosition.position, new Vector3(attackRangeX, attackRangeY, 1));
-    }
-
     private void Walk(Vector2 dir)
     {
         rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
@@ -109,24 +84,13 @@ public class PlayerController : MonoBehaviour
             anim.SetBool("isRunning", true);
         else
             anim.SetBool("isRunning", false);
-
-        if(!facingRight && dir.x > 0)
-        {
-            Flip();
-        }
-        else if(facingRight && dir.x < 0)
-        {
-            Flip();
-        }
-    }
-
-    private void Flip()
-    {
-        facingRight = !facingRight;
-        Vector3 Scaler = transform.localScale;
-        Scaler.x *= -1;
-        transform.localScale = Scaler;
-    }
+        
+        if(rb.velocity.x > 0)
+            spriteR.flipX = false;
+        else if(rb.velocity.x < 0)
+            spriteR.flipX = true;
+    
+    }   
 
     private void Jump()
     {
@@ -140,19 +104,5 @@ public class PlayerController : MonoBehaviour
         rb.velocity = Vector2.zero;
         Vector2 dir =  new Vector2(x, y);
         rb.velocity += dir.normalized * dashSpeed;
-    }
-
-    private void Attack()
-    {
-        Collider2D[] enemiesToDamage = Physics2D.OverlapBoxAll(
-            attackPosition.position,
-            new Vector2(attackRangeX, attackRangeY),
-            0,
-            whatIsEnemies
-        );
-        for(int i = 0; i < enemiesToDamage.Length; i++)
-        {
-            enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
-        }
     }
 }
