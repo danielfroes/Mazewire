722ee74c4e149e7753e48865950145907c62a41e
Daniel Fróes <danielfroes15@gmail.com>
2019-11-07 00:26:58 -0300

    draw health implementada

---
+++ MazewireC/Assets/HealthHeartsUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthHeartsUI : MonoBehaviour
+{
+
+    [SerializeField] private GameObject heart;
+    [SerializeField] private Sprite fullHeart;
+    [SerializeField] private Sprite emptyHeart;
+    
+    [SerializeField] private float distanceBetweenHearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DrawHearts(int life, int totalLife)
+    {
+        Image heartImgInstance;
+        for(int i = 0; i < totalLife; i ++)
+        {
+            heartImgInstance = Instantiate(heart, transform).GetComponent<Image>();
+            heartImgInstance.rectTransform.anchoredPosition = new Vector3(heartImgInstance.rectTransform.anchoredPosition.x + i * distanceBetweenHearts,
+                                                           heartImgInstance.rectTransform.anchoredPosition.y,
+                                                           0);
+            if(i < life)
+            {
+                heartImgInstance.sprite = fullHeart;
+            }
+            else
+            {
+                heartImgInstance.sprite = emptyHeart;
+            }
+            
+        }
+    }
+}
---
+++ MazewireC/Assets/PlayerLife.cs
@@ -0,0 +1,37 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+
+
+
+    [SerializeField] private int totalHealth= 3;
+    private int health;
+    public bool inCombat = true;
+    [SerializeField] private HealthHeartsUI heartsUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 2;
+        heartsUI.DrawHearts(health, totalHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!inCombat)
+        {
+            
+        }
+    }
+
+    public void TakeDamage()
+    {
+        health --;
+        // heartsUI.DrawHearts(health, totalHealth);
+    }
+
+}
